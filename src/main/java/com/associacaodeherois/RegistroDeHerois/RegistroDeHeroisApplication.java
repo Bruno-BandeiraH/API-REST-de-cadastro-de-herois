b0b37700ab1253254c88cd9c45472db86e079284
Bruno <brunohenrique0665@gmail.com>
2023-12-06 00:20:35 -0300

    first commit

---
+++ src/main/java/com/associacaodeherois/RegistroDeHerois/RegistroDeHeroisApplication.java
@@ -0,0 +1,13 @@
+package com.associacaodeherois.RegistroDeHerois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeHeroisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeHeroisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/associacaodeherois/RegistroDeHerois/RegistroDeHeroisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.associacaodeherois.RegistroDeHerois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeHeroisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
